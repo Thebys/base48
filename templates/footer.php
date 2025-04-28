d22ac585e86483f3489ae69691429be58df26ad0
Thebys <thebys@iver.cz>
2025-04-28 22:56:39 +0200

    fix content

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
     <div class="base48-building-container">
         <img id="base48-building" src="/img/base48_pixel_3_day_v2.webp" alt="Base48 Building">
     </div>
-    
+
     <div class="footer-container">
         <div class="footer-left">
             <h3>Informace</h3>
@@ -11,7 +11,8 @@
                 IČ: 22859802<br>
                 Registrováno: 6. 8. 2010
             </p>
-            <p>Bankovní účet:<br><a href="https://ib.fio.cz/ib/transparent?a=2900086515" target="_blank">2900086515/2010</a>
+            <p>Bankovní účet:<br><a href="https://ib.fio.cz/ib/transparent?a=2900086515"
+                    target="_blank">2900086515/2010</a>
             </p>
             <p>&copy; <?php echo date('Y'); ?> Base48, z.s.<br> Některá práva vyhrazena.</p>
         </div>
@@ -19,9 +20,7 @@
         <div class="footer-right">
             <h3>Partneři</h3>
             <p><a href="https://vpsfree.cz" target="_blank"><img src="/img/logo/vpsfree.png" alt="vpsfree.cz"
-                        style="width: 100px; height: auto;"></a></p>
-            <p>
-                <a href="https://github.com/base48" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+                        style="width: 100px; height: auto;"></a>
             </p>
         </div>
     </div>
